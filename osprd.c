a06f837a4698fb4db4c4b9d138faefe8ceb1d6d8
Austin Nasso <austinasso@gmail.com>
2015-05-14 16:57:27 -0400

    5/14 1:57

--- osprd.c
+++ osprd.c
@@ -274,8 +274,12 @@ int osprd_ioctl(struct inode *inode, struct file *filp,
         
         //CHECK FOR DEADLOCK FROM CURRENT = READ LOCK
         struct read_list *tmp1 = d->read_pids;
-        while (i < d->num_read_locks || tmp1->pid == current->pid)
+        int j = 0;
+        while (j < d->num_read_locks || tmp1->pid == current->pid)
+        {
             tmp1 = tmp1->next;
+            j++;
+        }
         
         if (tmp1->pid == current->pid)
         {
@@ -322,7 +326,10 @@ int osprd_ioctl(struct inode *inode, struct file *filp,
             {
                 tmp = d->read_pids;
                 while (i < d->num_read_locks)
+                {
                     tmp = tmp->next;
+                    i++;
+                }
             }
             
             tmp = kmalloc(sizeof(read_list*));
